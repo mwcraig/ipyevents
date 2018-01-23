233ba4d5649fbd76051c1966c92585a4421de55c
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-01-23 15:57:17 +0100

    Restore Python version to original and remove unnecessary view version

--- ipyevents/_version.py
+++ ipyevents/_version.py
@@ -1,7 +1,7 @@
 # What a full version specifier looks like:
 # version_info = (0, 0, 1, 'alpha', 0)
 
-version_info = (1, 0, 0, 'final', 0)
+version_info = (0, 0, 2, 'dev', 1)
 
 _specifier_ = {
         'dev': 'dev',
@@ -13,3 +13,5 @@ _specifier_ = {
 
 __version__ = '%s.%s.%s%s'%(version_info[0], version_info[1], version_info[2],
   '' if version_info[3]=='final' else _specifier_[version_info[3]]+str(version_info[4]))
+
+JS_EXTENSION_VERSION = '1.0.0'
