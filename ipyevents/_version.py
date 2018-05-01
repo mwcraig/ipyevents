b101f16f263b99b398b1eea57fc753bc29116ec6
Matthew Craig <mattwcraig@gmail.com>
2018-05-01 10:57:35 -0400

    Version 0.3.0 (npm 1.3.0)

--- ipyevents/_version.py
+++ ipyevents/_version.py
@@ -1,7 +1,7 @@
 # What a full version specifier looks like:
 # version_info = (0, 0, 1, 'alpha', 0)
 
-version_info = (0, 3, 0, 'dev', 1)
+version_info = (0, 3, 0, 'final', 0)
 
 _specifier_ = {
         'dev': 'dev',
