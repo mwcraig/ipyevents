282f5c3b2a573e133a374114a11d7798ff223c78
Matthew Craig <mattwcraig@gmail.com>
2018-11-20 14:36:55 -0600

    Update version to 0.3.2/1.3.2

--- ipyevents/_version.py
+++ ipyevents/_version.py
@@ -1,7 +1,7 @@
 # What a full version specifier looks like:
 # version_info = (0, 0, 1, 'alpha', 0)
 
-version_info = (0, 4, 0, 'dev', 0)
+version_info = (0, 3, 2, 'final', 0)
 
 _specifier_ = {
         'dev': 'dev',
