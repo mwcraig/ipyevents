670a853202a24b8a8ee41c7f9b6d780b044d47f8
martinRenou <martin.renou@gmail.com>
2021-04-07 14:10:50 +0200

    Update for JupyterLab 3

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from os.path import join as pjoin
+from os import path
 
-from setupbase import (
+from jupyter_packaging import (
     create_cmdclass, install_npm, ensure_targets,
-    find_packages, combine_commands, ensure_python,
-    get_version, HERE
+    combine_commands, ensure_python,
+    get_version, skip_if_exists
 )
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 LONG_DESCRIPTION = 'A custom widget for returning mouse and keyboard events to Python'
+
+HERE = path.dirname(path.abspath(__file__))
 
 # The name of the project
 name = 'ipyevents'
@@ -17,38 +19,44 @@ name = 'ipyevents'
 ensure_python('>=3.6')
 
 # Get our version
-version = get_version(pjoin(name, '_version.py'))
+version = get_version(path.join(name, '_version.py'))
 
-nb_path = pjoin(HERE, name, 'nbextension', 'static')
-lab_path = pjoin(HERE, name, 'labextension')
+nb_path = path.join(HERE, name, 'nbextension', 'static')
+lab_path = path.join(HERE, name, 'labextension')
 
 # Representative files that should exist after a successful build
 jstargets = [
-    pjoin(nb_path, 'index.js'),
-    pjoin(HERE, 'lib', 'plugin.js'),
+    path.join(nb_path, 'index.js'),
+    path.join(lab_path, 'package.json'),
 ]
 
 package_data_spec = {
     name: [
         'nbextension/static/*.*js*',
-        'labextension/*.tgz'
+        'labextension/**'
     ]
 }
 
 data_files_spec = [
     ('share/jupyter/nbextensions/ipyevents',
         nb_path, '*.js*'),
-    ('share/jupyter/lab/extensions', lab_path, '*.tgz'),
+    ('share/jupyter/labextensions/ipyevents', lab_path, '**'),
     ('etc/jupyter/nbconfig/notebook.d', HERE, 'ipyevents.json')
 ]
 
 
 cmdclass = create_cmdclass('jsdeps', package_data_spec=package_data_spec,
                            data_files_spec=data_files_spec)
-cmdclass['jsdeps'] = combine_commands(
-    install_npm(HERE, build_cmd='build:all'),
+js_command = combine_commands(
+    install_npm(HERE, build_cmd='build'),
     ensure_targets(jstargets),
 )
+
+is_repo = path.exists(path.join(HERE, '.git'))
+if is_repo:
+    cmdclass['jsdeps'] = js_command
+else:
+    cmdclass['jsdeps'] = skip_if_exists(jstargets, js_command)
 
 setup_args = dict(
     name            = name,
@@ -70,11 +78,13 @@ setup_args = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Framework :: Jupyter',
     ],
     include_package_data = True,
     install_requires = [
-        'ipywidgets>=7.0.0',
+        'ipywidgets>=7.6.0',
     ],
     extras_require = {
         'test': [
