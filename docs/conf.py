cefa3e8952b6c2ae60c7e9980b2b1f33b7ea630c
Matt Craig <mattwcraig@gmail.com>
2021-07-15 10:12:59 -0500

    Clarify some of the notebook and readme and add more binder links

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,34 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+nbsphinx_prolog = r"""
+{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
+.. raw:: html
+    <div class="admonition note">
+      This page was generated from
+      <a class="reference external" href="https://github.com/mwcraig/ipevents/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
+      Interactive online version:
+      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/mwcraig/ipevents/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
+      <script>
+        if (document.location.host) {
+          $(document.currentScript).replaceWith(
+            '<a class="reference external" ' +
+            'href="https://nbviewer.jupyter.org/url' +
+            (window.location.protocol == 'https:' ? 's/' : '/') +
+            window.location.host +
+            window.location.pathname.slice(0, -4) +
+            'ipynb">View in <em>nbviewer</em></a>.'
+          );
+        }
+      </script>
+    </div>
+.. raw:: latex
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+"""
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
--- docs/index.rst
+++ docs/index.rst
@@ -6,6 +6,16 @@
 ipyevents: add richer interaction to your Jupyter widgets
 =========================================================
 
+The best way to learn to use this package is to try it out on binder;
+it is really the only way to see the interactivity that ipyevents adds.
+
+Try it here:
+
+.. image:: https://mybinder.org/badge_logo.svg
+ :target: https://mybinder.org/v2/gh/mwcraig/ipevents/HEAD?filepath=docs%2Fevents.py
+
+A complete walk-through of the package is below.
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
